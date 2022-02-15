083572946e0df13d30f51c9abcb82baf75e5fdd5
nicola <nicolab0405@tlen.pl>
2022-02-15 21:12:37 +0100

    First Commit

---
+++ zad2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	
+	int liczba1,liczba2;
+	
+	cout<<"Podaj dwie liczby"<<endl;
+	cin>>liczba1>>liczba2;
+	
+	if (liczba1>=liczba2) {
+		cout<<liczba1;
+		
+	}
+	else if (liczba2>=liczba1) {
+		cout<<liczba2;
+		
+	}
+	
+	
+	return 0;
+	
+}
